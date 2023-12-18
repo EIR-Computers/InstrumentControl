1bde0564662e6cdc8956b9be88363dd3f270c66b
VF <e.i.radostev@gmail.com>
2023-12-18 10:02:01 +0300

    Rework

--- SDS1202Measure.py
+++ SDS1202Measure.py
@@ -4,7 +4,7 @@ import string
 import winsound
 
 
-#Function to select Horisontal scale by frequency
+# Function to select Horisontal scale by frequency
 def SelectHorScale(frequency):
     HORmode = 0
     HorModes = ['2.0ns', '5.0ns', '10.0ns', '20.0ns', '50.0ns', '100ns', '200ns', '500ns', '1.0us', '2.0us', '5.0us',
@@ -20,12 +20,13 @@ def SelectHorScale(frequency):
               50E+3, 100E+3,
               200E+3, 500E+3, 1000E+3]
     dur = (((1 / frequency) * periods * 1000) / cells)
-    for i in range(len(hscale)-1):
-        if dur >= hscale[i] and dur < hscale[i+1]:
-            HORmode=i+1
+    for i in range(len(hscale) - 1):
+        if dur >= hscale[i] and dur < hscale[i + 1]:
+            HORmode = i + 1
     return HorModes[HORmode]
 
-#Function to covert data from query about freq
+
+# Function to covert data from query about freq
 def InputToFreq(input):
     S1 = input[4:len(input)]
     index = S1.find('k')
@@ -36,10 +37,11 @@ def InputToFreq(input):
         index2 = S1.find('Hz')
         S1 = S1[0:index2]
     if S1.find('?') != -1:
-        S1='0'
+        S1 = '0'
     return float(S1)
 
-#Function to convert Voltage
+
+# Function to convert Voltage
 def InputToVTG(input):
     S1 = input[5:len(input)]
     indexV = S1.find('V')
@@ -50,106 +52,102 @@ def InputToVTG(input):
     if indexMV > 0:
         S1 = S1[0:indexMV]
     if S1.find('?') != -1:
-        S1='0'
+        S1 = '0'
     return float(S1)
 
-#defining number of measures
 
-TIME=int(input("ENTER duration of experiment in seconds: "))
-NUM=int(input("ENTER number of measurements: "))
+# defining number of measures
 
-stime = (TIME/NUM)/2
+TIME = int(input("ENTER duration of experiment in seconds: "))
+NUM = int(input("ENTER number of measurements: "))
+
+stime = (TIME / NUM) / 2
 if stime > 0.1:
-    print("Delay time is: " + str(TIME/NUM))
+    print("Delay time is: " + str(TIME / NUM))
 else:
     print("Delay OFF")
 
 time.sleep(1)
 
+# Init resource manager
+rm = pyvisa.ResourceManager()
 
-#Init resource manager
-rm=pyvisa.ResourceManager()
-
-#opening file
+# opening file
 file = open('measure2.txt', 'w')
 
-#creating obj DSO
-dso=rm.open_resource('USB0::0x5345::0x1234::SERIAL::INSTR')
-
+# creating obj DSO
+dso = rm.open_resource('USB0::0x5345::0x1234::SERIAL::INSTR')
 
 print("Query ID number:")
-#test conn
+# test conn
 print(dso.query('*IDN?'))
 
 time.sleep(1)
 
-#initial setup vert scale
-dso.write(':CH1:SCALe 20mV') #Attenuation is 10x
-#dso.write(':CH1:SCALe 200mV') #Attenuation is 10x
+# initial setup vert scale
+dso.write(':CH1:SCALe 20mV')  # Attenuation is 10x
+# dso.write(':CH1:SCALe 200mV') #Attenuation is 10x
 
 
 frequency = float(input("ENTER START FREQ in HZ: "))
 
-#initial measurement
+# initial measurement
 hormode = SelectHorScale(frequency)
 
 dso.write(':HORIzontal:SCALe ' + hormode)
 
-OUTFREQ='freq=['
-OUTVTG='vtg=['
-
-
+OUTFREQ = 'freq=['
+OUTVTG = 'vtg=['
 
 print("Executing cycle, wait")
 
 tic = time.perf_counter()
-for count in range (NUM):
-    #GENERATE SOUND (INCOMPATABLE)
-    #winsound.Beep(freq[count], 1000)
+for count in range(NUM):
+    # GENERATE SOUND (INCOMPATABLE)
+    # winsound.Beep(freq[count], 1000)
 
     # taking freq data
     frequencySTR = dso.query(':MEASUrement:CH1:FREQ?')
     frequency = InputToFreq(frequencySTR)
 
-    #if frequency is correctly measured - correct horisontal resolution of DSO
+    # if frequency is correctly measured - correct horisontal resolution of DSO
     if frequency != 0:
         hormode = SelectHorScale(frequency)
         dso.write(':HORIzontal:SCALe ' + hormode)
 
-    #taking voltage data
+    # taking voltage data
     VTGSTR = dso.query(':MEASUrement:CH1:VAMP?')
-    volts=InputToVTG(VTGSTR)
+    volts = InputToVTG(VTGSTR)
 
-    OUTFREQ+=str(frequency)+', '
-    OUTVTG+=str(volts)+', '
+    OUTFREQ += str(frequency) + ', '
+    OUTVTG += str(volts) + ', '
 
-    if count == round(NUM/4):
+    if count == round(NUM / 4):
         print("25% ELAPSED")
 
-    if count == round(NUM/2):
+    if count == round(NUM / 2):
         print("50% ELAPSED")
 
-    if count == round((NUM/4)*3):
+    if count == round((NUM / 4) * 3):
         print("75% ELAPSED")
 
-    #delay for instrument
+    # delay for instrument
     if stime > 0.1:
         time.sleep(stime)
     else:
         time.sleep(0.1)
 
+# formatting out str
+OUTVTG = OUTVTG[:len(OUTVTG) - 2] + '];'
+OUTFREQ = OUTFREQ[:len(OUTFREQ) - 2] + '];'
 
-#formatting out str
-OUTVTG=OUTVTG[:len(OUTVTG)-2]+'];'
-OUTFREQ=OUTFREQ[:len(OUTFREQ)-2]+'];'
-
-#Write to file
+# Write to file
 file.write(OUTVTG)
 file.write('\n')
 file.write(OUTFREQ)
 file.close()
 print('WRT-OK')
 
-#count time
+# count time
 toc = time.perf_counter()
 print(f" {toc - tic:0.4f} seconds elapsed for " + str(NUM) + ' measurements')
